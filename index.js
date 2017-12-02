27f9b31d99deb714d44e4692a35d3391d8a48ed7
Bethany  Walker <bethanymarciawalker@outlook.com>
2017-12-02 00:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, recipes, { [key]: value} );
+  return Object.assign({}, object, { [key]: value} );
   return recipes;
+  return updateObjectWithKeyAndValue(recipes, key, value);
 }
