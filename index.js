52e9249f798a0f8c3fae125d35cc0c0757cd78e3
Bethany  Walker <bethanymarciawalker@outlook.com>
2017-12-02 01:42:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
-
+deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object)
+  delete newObject[key];
+  return newObject;
+}
